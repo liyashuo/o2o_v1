14f91f8a968720c75e0a5d7dbbaf55436270efc9
yaron_lee <742847901@qq.com>
2023-04-05 10:27:06 +0800

    提交o2o部分实体类和相关sql脚本

---
+++ src/main/java/com/immoc/o2o/entity/ProductCategory.java
@@ -0,0 +1,58 @@
+package com.immoc.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * 商品类别
+ */
+public class ProductCategory {
+
+    private Long productCategoryId;
+    //shopId也是外键，对应Shop类里面的id
+    private Long shopId;
+    private String productCategoryName;
+
+    //权重，排序用
+    private Integer priority;
+    private Date createTime;
+
+    public Long getProductCategoryId() {
+        return productCategoryId;
+    }
+
+    public void setProductCategoryId(Long productCategoryId) {
+        this.productCategoryId = productCategoryId;
+    }
+
+    public Long getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Long shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getProductCategoryName() {
+        return productCategoryName;
+    }
+
+    public void setProductCategoryName(String productCategoryName) {
+        this.productCategoryName = productCategoryName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
--- src/main/java/com/immoc/o2o/sql/all.sql
+++ src/main/java/com/immoc/o2o/sql/all.sql
@@ -72,3 +72,119 @@ create table `tb_local_auth`
 # 这里增加的是索引，索引能增加查询的性能，但索引过多会影响数据库的性能
 alter table tb_wechat_auth
     add unique index (open_id);
+
+create table `tb_head_line`
+(
+    `line_id`        int(100)      NOT NULL AUTO_INCREMENT,
+    `line_name`      varchar(1000) NOT NULL,
+    `line_link`      varchar(2000) NOT NULL,
+    `line_img`       varchar(2000) NOT NULL,
+    `priority`       int(2)                 DEFAULT NULL,
+    `enable_status`  int(2)        NOT NULL DEFAULT '0',
+    `create_time`    datetime               DEFAULT NULL,
+    `last_edit_time` datetime               DEFAULT NULL,
+
+    primary key (`line_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+create table `tb_shop_category`
+(
+    `shop_category_id`   int(11)      NOT NULL AUTO_INCREMENT,
+    `shop_category_name` varchar(100) NOT NULL DEFAULT '',
+    `shop_category_desc` varchar(1000)         DEFAULT '',
+    `shop_category_img`  varchar(2000)         DEFAULT NULL,
+    `priority`           int(2)       NOT NULL DEFAULT '0',
+    `create_time`        datetime              DEFAULT NULL,
+    `last_edit_time`     datetime              DEFAULT NULL,
+    `parent_id`          int(11)               DEFAULT NULL,
+    primary key (`shop_category_id`),
+#     增加外键约束，并与哪个表做关联
+    constraint `fk_shop_category_self` foreign key (`parent_id`) references `tb_shop_category` (`shop_category_id`)
+
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+create table `tb_shop`
+(
+    `shop_id`          int(10)      NOT NULL AUTO_INCREMENT,
+    `owner_id`         int(10)      NOT NULL COMMENT '店铺创建人',
+    `area_id`          int(5)                DEFAULT NULL,
+    `shop_category_id` int(11)               DEFAULT NULL,
+    `shop_name`        varchar(256) NOT NULL,
+    `shop_desc`        varchar(1024)         DEFAULT '',
+    `shop_addr`        varchar(200)          DEFAULT NULL,
+    `phone`            varchar(128)          DEFAULT NULL,
+    `shop_img`         varchar(1024)         DEFAULT NULL,
+    `priority`         int(3)                DEFAULT '0',
+    `create_time`      datetime              DEFAULT NULL,
+    `last_edit_time`   datetime              DEFAULT NULL,
+    `enable_status`    int(2)       NOT NULL DEFAULT '0',
+    `advice`           varchar(255)          DEFAULT NULL,
+    primary key (`shop_id`),
+#     增加外键约束，并与哪个表做关联
+    constraint `fk_shop_area` foreign key (`area_id`) references `tb_area` (`area_id`),
+    constraint `fk_shop_profile` foreign key (`owner_id`) references `tb_person_info` (`user_id`),
+    constraint `fk_shop_shopcate` foreign key (`shop_category_id`) references `tb_shop_category` (`shop_category_id`)
+
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+create table `tb_product_category`
+(
+    `product_category_id`   int(11)      NOT NULL AUTO_INCREMENT,
+    `product_category_name` varchar(100) NOT NULL,
+    `priority`              int(2)                DEFAULT '0',
+    `create_time`           datetime              DEFAULT NULL,
+    `shop_id`               int(20)      NOT NULL DEFAULT '0',
+    primary key (`product_category_id`),
+#     增加外键约束，并与哪个表做关联
+    constraint `fk_procate_shop` foreign key (`shop_id`) references `tb_shop` (`shop_id`)
+
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+
+create table `tb_product`
+(
+    `product_id`          int(100)     NOT NULL AUTO_INCREMENT,
+    `product_name`        varchar(100) NOT NULL,
+    `product_desc`        varchar(2000)         DEFAULT '',
+    `img_addr`            varchar(2000)         DEFAULT '',
+    `normal_price`        varchar(100)          DEFAULT NULL,
+    `promotion_price`     varchar(100)          DEFAULT NULL,
+    `shop_img`            varchar(1024)         DEFAULT NULL,
+    `priority`            int(2)       NOT NULL DEFAULT '0',
+    `create_time`         datetime              DEFAULT NULL,
+    `last_edit_time`      datetime              DEFAULT NULL,
+    `enable_status`       int(2)       NOT NULL DEFAULT '0',
+    `product_category_id` int(11)               DEFAULT '0',
+    `shop_id`             int(20)      NOT NULL DEFAULT '0',
+    primary key (`product_id`),
+#     增加外键约束，并与哪个表做关联
+    constraint `fk_product_procate` foreign key (`product_category_id`) references `tb_product_category` (`product_category_id`),
+    constraint `fk_product_shop` foreign key (`shop_id`) references `tb_shop` (`shop_id`)
+
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+create table `tb_product_img`
+(
+    `product_img_id` int(20)       NOT NULL AUTO_INCREMENT,
+    `img_addr`       varchar(2000) NOT NULL,
+    `img_desc`       varchar(2000) NOT NULL,
+    `priority`       int(2)   DEFAULT '0',
+    `create_time`    datetime DEFAULT NULL,
+    `product_id`     int(20)  DEFAULT NULL,
+    primary key (`product_img_id`),
+#     增加外键约束，并与哪个表做关联
+    constraint `fk_proimg_product` foreign key (`product_id`) references `tb_product` (`product_id`)
+
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
